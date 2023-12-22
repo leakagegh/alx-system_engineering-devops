c286fda9b6a76dc6fcdb6963125ddd8708221be9
leakagegh <thehealer184@gmail.com>
2023-12-22 08:05:45 -0800

    SPECIAL

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Using Puppet to make changes to our configuration file.
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "
+  	  #ssh client configuration
+	  host*
+	  IdentityFile ~/.ssh/school
+	  PasswordAuthentication no
+	  ",
+}
